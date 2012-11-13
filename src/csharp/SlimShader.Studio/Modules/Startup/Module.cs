7a35164d25c1edd8d9c8103ef8b5d2d0eb15906a
Tim Jones <tim@timjones.tw>
2012-11-13 23:26:01 +0800

    Added SlimShader.Studio project

---
+++ src/csharp/SlimShader.Studio/Modules/Startup/Module.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using Gemini.Framework;
+
+namespace SlimShader.Studio.Modules.Startup
+{
+	[Export(typeof(IModule))]
+	public class Module : ModuleBase
+	{
+		public override void Initialize()
+		{
+			Shell.Title = "SlimShader Studio";
+		}
+	}
+}
