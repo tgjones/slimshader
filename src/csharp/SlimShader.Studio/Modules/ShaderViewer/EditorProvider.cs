b8bc7a0a16f1b42a1283a91b88700f026ce2a17a
Tim Jones <tim@timjones.tw>
2012-11-13 23:39:20 +0800

    Added basic UI for viewing disassembled code

---
+++ src/csharp/SlimShader.Studio/Modules/ShaderViewer/EditorProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+using Gemini.Framework;
+using Gemini.Framework.Services;
+using SlimShader.Studio.Modules.ShaderViewer.ViewModels;
+
+namespace SlimShader.Studio.Modules.ShaderViewer
+{
+	[Export(typeof(IEditorProvider))]
+	public class EditorProvider : IEditorProvider
+	{
+		public bool Handles(string path)
+		{
+			return true;
+		}
+
+		public IDocument Create(string path)
+		{
+			var editor = new EditorViewModel();
+			editor.Open(path);
+			return editor;
+		}
+	}
+}
---
+++ src/csharp/SlimShader.Studio/Modules/ShaderViewer/ViewModels/EditorViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using Gemini.Framework;
+
+namespace SlimShader.Studio.Modules.ShaderViewer.ViewModels
+{
+	public class EditorViewModel : Document
+	{
+		private string _path;
+		private string _fileName;
+		private BytecodeContainer _bytecodeContainer;
+		private string _disassembledCode;
+
+		public override string DisplayName
+		{
+			get { return _fileName; }
+		}
+
+		public string DisassembledCode
+		{
+			get { return _disassembledCode; }
+			private set
+			{
+				_disassembledCode = value;
+				NotifyOfPropertyChange(() => DisassembledCode);
+			}
+		}
+
+		public void Open(string path)
+		{
+			_path = path;
+			_fileName = Path.GetFileName(path);
+			_bytecodeContainer = BytecodeContainer.Parse(File.ReadAllBytes(path));
+			DisassembledCode = _bytecodeContainer.ToString();
+		}
+
+		public override bool Equals(object obj)
+		{
+			var other = obj as EditorViewModel;
+			return other != null && string.Compare(_path, other._path) == 0;
+		}
+	}
+}
