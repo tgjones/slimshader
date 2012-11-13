8e5afc651197b235dc7c45bb344d9f370ee168ec
Tim Jones <tim@timjones.tw>
2012-11-14 00:11:37 +0800

    Added "programmer art" icon

--- src/csharp/SlimShader.Studio/Modules/Startup/Module.cs
+++ src/csharp/SlimShader.Studio/Modules/Startup/Module.cs
@@ -1,14 +1,27 @@
 ﻿using System.ComponentModel.Composition;
+using System.Reflection;
+using Caliburn.Micro;
 using Gemini.Framework;
+using Gemini.Framework.Services;
 
 namespace SlimShader.Studio.Modules.Startup
 {
 	[Export(typeof(IModule))]
 	public class Module : ModuleBase
 	{
+		private readonly IResourceManager _resourceManager;
+
+		[ImportingConstructor]
+		public Module(IResourceManager resourceManager)
+		{
+			_resourceManager = resourceManager;
+		}
+
 		public override void Initialize()
 		{
 			Shell.Title = "SlimShader Studio";
+			Shell.Icon = _resourceManager.GetBitmap("Resources/Icon.ico",
+				Assembly.GetExecutingAssembly().GetAssemblyName());
 		}
 	}
 }
