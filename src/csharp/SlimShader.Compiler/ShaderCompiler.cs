fd8107d78e54fff35e06d86a605e76ad6ebcb17b
Tim Jones <tim@timjones.tw>
2012-11-26 22:35:46 +0800

    Add SlimShader.Compiler (wrapper around D3D)

---
+++ src/csharp/SlimShader.Compiler/ShaderCompiler.cs
@@ -0,0 +1,13 @@
+﻿using SharpDX.D3DCompiler;
+
+namespace SlimShader.Compiler
+{
+	public static class ShaderCompiler
+	{
+		public static BytecodeContainer CompileFromFile(string fileName, string entryPoint, string profile)
+		{
+			var compiledShader = ShaderBytecode.CompileFromFile(fileName, entryPoint, profile);
+			return BytecodeContainer.Parse(compiledShader.Bytecode.Data);
+		}
+	}
+}
