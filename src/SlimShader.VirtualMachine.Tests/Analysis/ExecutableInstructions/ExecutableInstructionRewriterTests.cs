f2ad5c14f8e69abbce5d80a5ceda6c437e02f167
Tim Jones <tim@timjones.tw>
2013-04-27 23:39:34 +0800

    Add executable instruction rewriter test

--- src/SlimShader.VirtualMachine.Tests/Analysis/ExecutableInstructions/ExecutableInstructionRewriterTests.cs
+++ src/SlimShader.VirtualMachine.Tests/Analysis/ExecutableInstructions/ExecutableInstructionRewriterTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using SlimShader.Chunks.Shex.Tokens;
 using SlimShader.VirtualMachine.Analysis.ControlFlow;
 using SlimShader.VirtualMachine.Analysis.ExecutableInstructions;
 using SlimShader.VirtualMachine.Analysis.ExplicitBranching;
@@ -16,15 +17,37 @@ namespace SlimShader.VirtualMachine.Tests.Analysis.ExecutableInstructions
 			// Arrange.
 			var instructions = new List<InstructionBase>
 			{
-				new NormalInstruction { }
+				CreateInstruction<NormalInstruction>(),    // 0
+				CreateInstruction<BranchingInstruction>(), // 1 Unconditional branch to [3]
+				CreateInstruction<NormalInstruction>(),    // 2 Skipped
+				CreateInstruction<BranchingInstruction>(), // 3 Conditional branch to [5]
+				CreateInstruction<NormalInstruction>(),    // 4
+				CreateInstruction<NormalInstruction>()     // 5
 			};
-			var controlFlowGraph = new ControlFlowGraph(instructions);
+			((BranchingInstruction) instructions[1]).BranchType = BranchType.Unconditional;
+			((BranchingInstruction) instructions[1]).BranchTarget = instructions[3];
+			((BranchingInstruction) instructions[3]).BranchType = BranchType.Conditional;
+			((BranchingInstruction) instructions[3]).BranchTarget = instructions[5];
+			var controlFlowGraph = ControlFlowGraph.FromInstructions(instructions);
 
 			// Act.
 			var executableInstructions = ExecutableInstructionRewriter.Rewrite(controlFlowGraph).ToList();
 
 			// Assert.
-			Assert.That(executableInstructions, Has.Count.EqualTo(5));
+			Assert.That(executableInstructions, Has.Count.EqualTo(6));
+			Assert.That(((NonDivergentExecutableInstruction) executableInstructions[0]).NextPC, Is.EqualTo(1));
+			Assert.That(((NonDivergentExecutableInstruction) executableInstructions[1]).NextPC, Is.EqualTo(3));
+			Assert.That(((NonDivergentExecutableInstruction) executableInstructions[2]).NextPC, Is.EqualTo(3));
+			Assert.That(((DivergentExecutableInstruction) executableInstructions[3]).NextPCs, Is.EquivalentTo(new[] { 4, 5 }));
+			Assert.That(((DivergentExecutableInstruction) executableInstructions[3]).ReconvergencePC, Is.EqualTo(5));
+			Assert.That(((NonDivergentExecutableInstruction) executableInstructions[4]).NextPC, Is.EqualTo(5));
+			Assert.That(((NonDivergentExecutableInstruction) executableInstructions[5]).NextPC, Is.EqualTo(6));
+		}
+
+		private static T CreateInstruction<T>()
+			where T : InstructionBase, new()
+		{
+			return new T { InstructionToken = new InstructionToken() };
 		}
 	}
 }
