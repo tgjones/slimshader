776ddcf12ccc9fcf0d0978e692d78e231913bd59
Tim Jones <tim@timjones.tw>
2013-04-27 14:40:31 +0800

    Fix patch constant signature line ending

--- src/SlimShader/Chunks/Xsgn/PatchConstantSignatureChunk.cs
+++ src/SlimShader/Chunks/Xsgn/PatchConstantSignatureChunk.cs
@@ -1,12 +1,16 @@
-﻿namespace SlimShader.Chunks.Xsgn
+﻿using System.Text;
+
+namespace SlimShader.Chunks.Xsgn
 {
 	public class PatchConstantSignatureChunk : InputOutputSignatureChunk
 	{
 		public override string ToString()
 		{
-			return @"// Patch Constant signature:
-//
-" + base.ToString();
+			var sb = new StringBuilder();
+			sb.AppendLine("// Patch Constant signature:");
+			sb.AppendLine("//");
+			sb.Append(base.ToString());
+			return sb.ToString();
 		}
 	}
 }
