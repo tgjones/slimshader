fbcd661cf34c2ce50092043add5c18f116aea3c7
Tim Jones <tim@timjones.tw>
2013-08-09 21:50:01 +0800

    JITter now builds (empty) dynamic method

--- src/SlimShader.VirtualMachine.Jitter/JitShaderExecutor.cs
+++ src/SlimShader.VirtualMachine.Jitter/JitShaderExecutor.cs
@@ -1,6 +1,10 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
 using SlimShader.VirtualMachine.Analysis.ExecutableInstructions;
 using SlimShader.VirtualMachine.Execution;
 
@@ -38,17 +42,51 @@ namespace SlimShader.VirtualMachine.Jitter
 
         private static ExecuteShaderDelegate JitCompileShader(BytecodeContainer bytecode)
         {
-            var dynamicMethod = new DynamicMethod("ExecuteShader",
-                typeof(IEnumerable<ExecutionResponse>),
-                new[]
-                {
-                    typeof(VirtualMachine),
-                    typeof(ExecutionContext[]),
-                    typeof(ExecutableInstruction)
-                });
+            var assemblyReferences = new[]
+            {
+                MetadataReference.CreateAssemblyReference("mscorlib"),
+                new MetadataFileReference(typeof(VirtualMachine).Assembly.Location)
+            };
 
-            var ilGenerator = dynamicMethod.GetILGenerator();
-            ilGenerator.Emit(OpCodes.Ret);
+            var outputName = "DynamicShader";
+            var code = @"
+                using System.Collections.Generic;
+                using SlimShader.VirtualMachine;
+                using SlimShader.VirtualMachine.Analysis.ExecutableInstructions;
+
+                public static class DynamicShaderExecutor
+                {
+                    public static IEnumerable<ExecutionResponse> Execute(
+                        VirtualMachine virtualMachine, ExecutionContext[] executionContexts,
+                        ExecutableInstruction[] instructions)
+                    {
+                        // TODO
+                        yield return ExecutionResponse.Finished;
+                    }
+                }
+                ";
+            
+            var compilation = Compilation.Create(outputName)
+                .WithOptions(new CompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .AddReferences(assemblyReferences)
+                .AddSyntaxTrees(SyntaxTree.ParseText(code));
+
+            var moduleBuilder = AppDomain.CurrentDomain
+                .DefineDynamicAssembly(new AssemblyName(outputName),
+                    AssemblyBuilderAccess.RunAndCollect)
+                .DefineDynamicModule(outputName);
+
+            var compilationResult = compilation.Emit(moduleBuilder);
+            if (!compilationResult.Success)
+            {
+                var errorMessage = string.Empty;
+                foreach (var diagnostic in compilationResult.Diagnostics)
+                    errorMessage += diagnostic + Environment.NewLine;
+                throw new Exception(errorMessage);
+            }
+
+            var dynamicClass = moduleBuilder.GetType("DynamicShaderExecutor", false, true);
+            var dynamicMethod = dynamicClass.GetMethod("Execute");
 
             return (ExecuteShaderDelegate) dynamicMethod.CreateDelegate(typeof(ExecuteShaderDelegate));
         }
