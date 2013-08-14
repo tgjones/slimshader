65ec2931192d6264e85beacabb5eacce0d991780
Tim Jones <tim@timjones.tw>
2013-08-14 16:11:21 +0800

    Don't throw exception when semantic is not found

--- src/SlimShader/Chunks/Xsgn/SignatureParameterDescriptionCollection.cs
+++ src/SlimShader/Chunks/Xsgn/SignatureParameterDescriptionCollection.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 
@@ -6,12 +5,11 @@ namespace SlimShader.Chunks.Xsgn
 {
 	public class SignatureParameterDescriptionCollection : Collection<SignatureParameterDescription>
 	{
-		public uint FindRegister(string semanticName, uint semanticIndex)
+		public uint? FindRegister(string semanticName, uint semanticIndex)
 		{
 			var parameter = this.SingleOrDefault(x => x.SemanticName == semanticName && x.SemanticIndex == semanticIndex);
-			if (parameter == null)
-				throw new Exception(string.Format("No matching input parameter for semantic name '{0}' and index '{1}'.",
-					semanticName, semanticIndex));
+            if (parameter == null)
+                return null;
 			return parameter.Register;
 		}
 	}
