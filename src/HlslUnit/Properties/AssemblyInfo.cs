db45781109fa7e76ee853471eea220ea285b56e9
Tim Jones <tim@timjones.tw>
2014-01-07 22:33:20 +0800

    Make HlslUnit a portable class library

--- src/HlslUnit/Properties/AssemblyInfo.cs
+++ src/HlslUnit/Properties/AssemblyInfo.cs
@@ -1,26 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HlslUnit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("HlslUnit is a unit testing library for HLSL shaders")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tim Jones")]
 [assembly: AssemblyProduct("HlslUnit")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©2014 Tim Jones")]
+[assembly: AssemblyTrademark("timjones.tw")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d080ece-2172-4fa5-8da3-a1419953af13")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
